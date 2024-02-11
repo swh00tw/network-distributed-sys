82e47e79b839b43fbf777ad89d45aa6221d55525
joannechen1223 <joannechen1223@gmail.com>
2024-02-11 17:19:08 -0500

    add script to create large files

---
+++ lab1/HW1/create_file.py
@@ -0,0 +1,21 @@
+import sys
+
+
+def create_file(n):
+    with open(f'large_test_{n}.txt', 'w') as file:
+        for i in range(n):
+            line = ''
+            for _ in range(0, 128):
+                line += f'packet-{i:02d}'
+
+            # erase last character
+            line = line[:-1]
+            line += '\n'
+            file.write(line)
+        file.write('END.')
+
+
+if __name__ == '__main__':
+    # take first args as n
+    n = int(sys.argv[1])
+    create_file(n)
